98aa495609cc9361513b4d56f7c44a4b74640d49
Guilherme FGR <guifgr@hotmail.com>
2021-03-13 09:58:04 -0300

    [insertionSort]

--- Insertion/Program.cs
+++ Insertion/Program.cs
@@ -6,7 +6,32 @@ namespace Insertion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long[] x = new long[5];
+            long j, i, eleito;
+            
+            for (i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("o "+(i+1)+"º número: ");
+                x[i] = Convert.ToInt64(Console.ReadLine());
+            }
+
+            for (i = 1; i <= 4; i++)
+            {
+                eleito = x[i];
+                j = i - 1;
+                while ((j >= 0) &&
+                       (x[j] > eleito))
+                {
+                    x[j + 1] = x[j];
+                    j -= 1;
+                }
+                
+                x[j + 1] = eleito;
+            }
+            for (i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(i+1+"º número: "+x[i]);
+            }
         }
     }
 }
